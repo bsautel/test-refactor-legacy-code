4284521b1c0fd04dd0e99c3390581fe49bab6753
Benoit Sautel <benoit.sautel@gmail.com>
2013-12-03 21:42:42 +0100

    Refactor filtering of shared pictures

--- src/main/java/org/legacy/demo/picture/PictureService.java
+++ src/main/java/org/legacy/demo/picture/PictureService.java
@@ -1,10 +1,15 @@
 package org.legacy.demo.picture;
 
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Lists.newArrayList;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.legacy.demo.exception.UserNotLoggedInException;
 import org.legacy.demo.user.User;
+
+import com.google.common.base.Predicate;
 
 public class PictureService {
 	private final PictureDao pictureDao;
@@ -25,16 +30,22 @@ public class PictureService {
 				}
 			}
 			if (isFriend) {
-				List<Picture> userPicturesList = pictureDao.findPicturesByUser(user);
-				for (Picture picture : userPicturesList) {
-					if (picture.canBeShared()) {
-						picturesList.add(picture);
-					}
-				}
+				List<Picture> userPicturesList = pictureDao
+						.findPicturesByUser(user);
+				picturesList.addAll(filterSharedPicture(userPicturesList));
 			}
 			return picturesList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
+	}
+
+	private List<Picture> filterSharedPicture(List<Picture> pictures) {
+		return newArrayList(filter(pictures, new Predicate<Picture>() {
+			@Override
+			public boolean apply(Picture picture) {
+				return picture.canBeShared();
+			}
+		}));
 	}
 }
