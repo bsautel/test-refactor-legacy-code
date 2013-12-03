d5b38860374323f1725288468d272796f55c05c8
Benoit Sautel <benoit.sautel@gmail.com>
2013-12-03 21:19:35 +0100

    Add a test with two users that are not friend

--- src/test/java/org/legacy/demo/picture/PictureServiceTest.java
+++ src/test/java/org/legacy/demo/picture/PictureServiceTest.java
@@ -1,23 +1,47 @@
 package org.legacy.demo.picture;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.legacy.demo.exception.UserNotLoggedInException;
 import org.legacy.demo.user.User;
 
 public class PictureServiceTest {
+	private PictureService pictureService;
+	private User bob;
+	private User loggedInUser;
+
 	private class MyPictureService extends PictureService {
 		@Override
 		protected User getLoggedInUser() {
-			return null;
+			return loggedInUser;
 		}
+	}
+
+	@Before
+	public void setUp() {
+		pictureService = new MyPictureService();
+		bob = new User("Bob");
 	}
 
 	@Test(expected = UserNotLoggedInException.class)
 	public void shouldThrowAnExceptionWhenUserIsNotLoggedIn()
 			throws UserNotLoggedInException {
-		PictureService pictureService = new MyPictureService();
-		User bob = new User("Bob");
+		loggedInUser = null;
 
 		pictureService.getPicturesByUser(bob);
+	}
+
+	@Test
+	public void shoudReturnNoPictureWithTwoUsersThatAreNotFriend()
+			throws UserNotLoggedInException {
+		loggedInUser = new User("Alice");
+
+		List<Picture> pictures = pictureService.getPicturesByUser(bob);
+
+		assertTrue(pictures.isEmpty());
 	}
 }
